c8a0bb9a94398e7870f1a595a2914bb49c3eba64
b-nimai <nimaibarman4978@gmail.com>
2024-08-11 15:32:12 +0530

    AWS Test Repo

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express() 
+// const port = 3000 //drfining the port 
+
+// for extracting the request body from request
+const bodyParser = require("body-parser") 
+
+app.use(bodyParser.json()) //using body parser
+
+//creating a route
+app.get("/", (req, res) =>{  
+    const a = req.query("a")
+    const b = req.query("b")
+    const sum = parseInt(a) + parseInt(b);
+    return res.json({
+        a,
+        b,
+        sum
+    })
+})
+module.exports = app;
